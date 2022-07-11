ef383612ee16490f6ed6a03e9de8ac248d387a43
yusank <yusankurban@gmail.com>
2022-07-11 18:35:11 +0800

    fix: message

--- internal/service/msg_service.go
+++ internal/service/msg_service.go
@@ -23,7 +23,6 @@ import (
 	"github.com/go-goim/core/pkg/graceful"
 	"github.com/go-goim/core/pkg/log"
 	"github.com/go-goim/core/pkg/waitgroup"
-
 	"github.com/go-goim/msg-worker/internal/app"
 )
 
@@ -67,38 +66,88 @@ func (s *MqMessageService) Consume(ctx context.Context, msg ...*primitive.Messag
 	return consumer.ConsumeSuccess, nil
 }
 
-func (s *MqMessageService) handleSingleMsg(ctx context.Context, msg *primitive.MessageExt) error {
+func (s *MqMessageService) handleSingleMsg(ctx context.Context, ext *primitive.MessageExt) error {
 	// PushMessageReq contains all MqMessage fields.
-	req := &messagev1.MqMessage{}
-	if err := json.Unmarshal(msg.Body, req); err != nil {
+	msg := &messagev1.Message{}
+	if err := json.Unmarshal(ext.Body, msg); err != nil {
 		return err
 	}
 
-	in := &messagev1.PushMessageReq{
-		FromUser:    req.GetFromUser(),
-		ToUser:      req.GetToUser(),
-		SessionType: req.GetSessionType(),
-		ContentType: req.GetContentType(),
-		Content:     req.GetContent(),
-		MsgSeq:      msg.MsgId,
-	}
-
-	switch req.GetSessionType() {
+	switch msg.GetSessionType() {
 	case sessionv1.SessionType_SingleChat:
 		// push to single user
-		return s.msgToUser(ctx, in)
+		return s.msgToUser(ctx, msg, ext)
 	case sessionv1.SessionType_GroupChat:
 		// push to group
-		return s.toGroup(ctx, in)
+		return s.toGroup(ctx, msg, ext)
 	case sessionv1.SessionType_Broadcast:
 		// push to all
-		return s.broadcast(ctx, in)
+		return s.broadcast(ctx, msg, ext)
 	default:
-		return fmt.Errorf("unknown session type: %d", req.GetSessionType())
+		return fmt.Errorf("unknown session type: %d", msg.GetSessionType())
 	}
 }
 
-func (s *MqMessageService) broadcast(ctx context.Context, req *messagev1.PushMessageReq) error {
+func (s *MqMessageService) msgToUser(ctx context.Context, msg *messagev1.Message, ext *primitive.MessageExt) error {
+	agentIP, err := s.rdb.Get(ctx, consts.GetUserOnlineAgentKey(msg.GetTo())).Result()
+	if err != nil {
+		if err == redisv8.Nil {
+			log.Info("user offline, put to offline queue", "user_id", msg.GetTo())
+			// only when user is offline, put to offline queue
+			return s.putToRedis(ctx, msg.GetTo(), msg.GetMsgId(), ext.Body)
+		}
+		return err
+	}
+
+	// trim port from agentIP
+	agentIP = strings.Split(agentIP, ":")[0]
+
+	log.Info("user online, send to agent", "uid", msg.GetTo(), "agent_ip", agentIP)
+	cc, err := s.loadGrpcConn(ctx, agentIP)
+	if err != nil {
+		return err
+	}
+
+	out, err := messagev1.NewPushMessageServiceClient(cc).PushMessage(ctx, &messagev1.PushMessageReq{
+		Message: msg,
+		ToUsers: []string{msg.GetTo()},
+	})
+	if err != nil {
+		log.Info("MSG send msg err=", err.Error())
+		return err
+	}
+
+	if !out.Response.Success() {
+		_ = s.putToRedis(ctx, msg.GetTo(), msg.GetMsgId(), ext.Body)
+		return out.Response
+	}
+
+	return nil
+}
+
+func (s *MqMessageService) putToRedis(ctx context.Context, to string, msgID int64, body []byte) error {
+	key := consts.GetUserOfflineQueueKey(to)
+
+	// add to queue
+	pp := s.rdb.Pipeline()
+	_ = pp.Process(ctx, s.rdb.ZAdd(ctx, key, &redisv8.Z{
+		Score:  float64(msgID),
+		Member: string(body),
+	}))
+	// set key expire
+	_ = pp.Process(ctx, s.rdb.Expire(ctx, key, consts.UserOfflineQueueKeyExpire))
+	// trim old messages
+	_ = pp.Process(ctx, s.rdb.ZRemRangeByRank(ctx, key, 0, -int64(consts.UserOfflineQueueMemberMax+1)))
+
+	_, err := pp.Exec(ctx)
+	if err != nil {
+		log.Info("Exec pipeline err", "err", err.Error())
+	}
+
+	return nil
+}
+
+func (s *MqMessageService) broadcast(ctx context.Context, msg *messagev1.Message, _ *primitive.MessageExt) error {
 	list, err := app.GetApplication().Register.GetService(ctx, app.GetApplication().Config.SrvConfig.PushService)
 	if err != nil {
 		return err
@@ -110,7 +159,10 @@ func (s *MqMessageService) broadcast(ctx context.Context, req *messagev1.PushMes
 				continue
 			}
 
-			if err = s.broadcastToEndpoint(ctx, req, strings.TrimPrefix(ep, "grpc://")); err != nil {
+			if err = s.broadcastToEndpoint(ctx, strings.TrimPrefix(ep, "grpc://"), &messagev1.PushMessageReq{
+				Message: msg,
+				ToUsers: []string{"ALL"},
+			}); err != nil {
 				log.Info("broadcastToEndpoint err=", err)
 			}
 		}
@@ -119,7 +171,7 @@ func (s *MqMessageService) broadcast(ctx context.Context, req *messagev1.PushMes
 	return nil
 }
 
-func (s *MqMessageService) broadcastToEndpoint(ctx context.Context, req *messagev1.PushMessageReq, ep string) error {
+func (s *MqMessageService) broadcastToEndpoint(ctx context.Context, ep string, req *messagev1.PushMessageReq) error {
 	cc, err := grpc.DialInsecure(ctx, grpc.WithEndpoint(ep), grpc.WithTimeout(time.Second*5))
 	if err != nil {
 		return err
@@ -130,14 +182,14 @@ func (s *MqMessageService) broadcastToEndpoint(ctx context.Context, req *message
 		return err
 	}
 
-	if !rsp.Success() {
-		return rsp
+	if !rsp.Response.Success() {
+		return rsp.Response
 	}
 
 	return nil
 }
 
-func (s *MqMessageService) toGroup(ctx context.Context, req *messagev1.PushMessageReq) error {
+func (s *MqMessageService) toGroup(ctx context.Context, msg *messagev1.Message, ext *primitive.MessageExt) error {
 	// load group members
 	cc, err := s.userServiceCP.Get()
 	if err != nil {
@@ -145,7 +197,7 @@ func (s *MqMessageService) toGroup(ctx context.Context, req *messagev1.PushMessa
 	}
 
 	resp, err := grouppb.NewGroupServiceClient(cc).GetGroup(ctx, &grouppb.GetGroupRequest{
-		Gid:         req.GetToUser(),
+		Gid:         msg.GetTo(),
 		WithMembers: true,
 	})
 	if err != nil {
@@ -156,110 +208,90 @@ func (s *MqMessageService) toGroup(ctx context.Context, req *messagev1.PushMessa
 		return resp.Response
 	}
 
-	wg := waitgroup.NewWaitGroup(len(resp.GetGroup().GetMembers()) - 1)
-	// todo: list all online users then send rpc request to push server with batch instead of one by one
-	// push to group members
+	var members []string
 	for _, member := range resp.GetGroup().GetMembers() {
-		if member.GetUid() == req.GetFromUser() {
+		if member.GetUid() == msg.GetFrom() {
 			continue
 		}
-		req2 := &messagev1.PushMessageReq{
-			FromUser:    req.GetFromUser(),
-			ToUser:      member.GetUid(),
-			ToGroup:     &req.ToUser,
-			SessionType: req.GetSessionType(),
-			ContentType: req.GetContentType(),
-			Content:     req.GetContent(),
-			MsgSeq:      req.GetMsgSeq(),
-			SessionId:   req.GetSessionId(),
+		members = append(members, member.GetUid())
+	}
+
+	if len(members) == 0 {
+		return nil
+	}
+
+	onlineUserMap, offlineUsers, err := s.filterOnlineUsers(ctx, members)
+	if err != nil {
+		return err
+	}
+
+	pushFunc := func(agentIP string, users []string) error {
+		cc, err := s.loadGrpcConn(ctx, agentIP)
+		if err != nil {
+			return err
 		}
 
+		out, err := messagev1.NewPushMessageServiceClient(cc).PushMessage(ctx, &messagev1.PushMessageReq{
+			Message: msg,
+			ToUsers: users,
+		})
+		if err != nil {
+			log.Error("MSG send msg err", "err", err.Error())
+			return err
+		}
+
+		if !out.Response.Success() {
+			// put all to offline queue
+			offlineUsers = append(offlineUsers, users...)
+			return out.Response
+		}
+
+		// put failed users to offline queue
+		offlineUsers = append(offlineUsers, out.GetFailedUsers()...)
+		return nil
+	}
+
+	// push to online users
+	wg := waitgroup.NewWaitGroup(len(onlineUserMap) + 1)
+	for agentIP, users := range onlineUserMap {
 		wg.Add(func() {
-			if err = s.msgToUser(ctx, req2); err != nil {
-				log.Info("msgToUser", "err=", err, "req=", req2)
+			if err := pushFunc(agentIP, users); err != nil {
+				log.Error("push to agent err", "err", err.Error(), "agent_ip", agentIP)
 			}
 		})
 	}
-
 	wg.Wait()
 
+	// maybe put redis in batch be batter
+	for _, user := range offlineUsers {
+		_ = s.putToRedis(ctx, user, msg.GetMsgId(), ext.Body)
+	}
+
 	return nil
 }
 
-func (s *MqMessageService) msgToUser(ctx context.Context, req *messagev1.PushMessageReq) error {
-	agentIP, err := s.rdb.Get(ctx, consts.GetUserOnlineAgentKey(req.GetToUser())).Result()
-	if err != nil {
-		if err == redisv8.Nil {
-			log.Info("user offline, put to offline queue", "user_id", req.GetToUser())
-			// only when user is offline, put to offline queue
-			return s.putToRedis(ctx, req)
+func (s *MqMessageService) filterOnlineUsers(ctx context.Context, users []string) (
+	onlineUserMap map[string][]string, offlineUsers []string, err error) {
+
+	// key is agent ip, value is user id
+	onlineUserMap = make(map[string][]string)
+
+	for _, uid := range users {
+		agentIP, err := s.rdb.Get(ctx, consts.GetUserOnlineAgentKey(uid)).Result()
+		if err != nil {
+			if err == redisv8.Nil {
+				offlineUsers = append(offlineUsers, uid)
+				continue
+			}
+			return nil, nil, err
 		}
-		return err
+
+		// trim port from agentIP
+		agentIP = strings.Split(agentIP, ":")[0]
+		onlineUserMap[agentIP] = append(onlineUserMap[agentIP], uid)
 	}
 
-	// trim port from agentIP
-	agentIP = strings.Split(agentIP, ":")[0]
-
-	log.Info("user online, send to agent", "uid", req.GetToUser(), "agent_ip", agentIP)
-	cc, err := s.loadGrpcConn(ctx, agentIP)
-	if err != nil {
-		return err
-	}
-
-	out, err := messagev1.NewPushMessageServiceClient(cc).PushMessage(ctx, req)
-	if err != nil {
-		log.Info("MSG send msg err=", err.Error())
-		return err
-	}
-
-	if !out.Success() {
-		return out
-	}
-
-	return nil
-}
-
-func (s *MqMessageService) putToRedis(ctx context.Context, req *messagev1.PushMessageReq) error {
-	msgID, err := primitive.UnmarshalMsgID([]byte(req.MsgSeq))
-	if err != nil {
-		log.Info("unmarshal ext id err=", err)
-		return err
-	}
-	log.Info("unmarshal ext", "host", msgID.Addr, "port", msgID.Port, "offset", msgID.Offset)
-
-	msg := &messagev1.BriefMessage{
-		FromUser:    req.GetFromUser(),
-		ToUser:      req.GetToUser(),
-		ContentType: req.GetContentType(),
-		Content:     req.GetContent(),
-		MsgSeq:      req.MsgSeq,
-		SessionId:   req.GetSessionId(),
-	}
-
-	body, err := json.Marshal(msg)
-	if err != nil {
-		return err
-	}
-
-	key := consts.GetUserOfflineQueueKey(req.GetToUser())
-
-	// add to queue
-	pp := s.rdb.Pipeline()
-	_ = pp.Process(ctx, s.rdb.ZAdd(ctx, key, &redisv8.Z{
-		Score:  float64(msgID.Offset),
-		Member: string(body),
-	}))
-	// set key expire
-	_ = pp.Process(ctx, s.rdb.Expire(ctx, key, consts.UserOfflineQueueKeyExpire))
-	// trim old messages
-	_ = pp.Process(ctx, s.rdb.ZRemRangeByRank(ctx, key, 0, -int64(consts.UserOfflineQueueMemberMax+1)))
-
-	_, err = pp.Exec(ctx)
-	if err != nil {
-		log.Info("Exec pipeline err=", err)
-	}
-
-	return nil
+	return onlineUserMap, offlineUsers, nil
 }
 
 func (s *MqMessageService) loadGrpcConn(_ context.Context, agentIP string) (cc *cgrpc.ClientConn, err error) {
